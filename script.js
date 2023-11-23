371ad6883922dc52e0f5af2b80c03df527f927b5
giftedmelon <lyx1231996@gmail.com>
2023-11-23 09:55:53 +0900

    11

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ function changeLanguage(language) {
     const selectedGreeting = greetings[language] || greetings['en'];
     nameElement.textContent = selectedGreeting;
   }
+
+
+  /*function changeLanguage(language) {
+    const nameElement = document.getElementById('myName');
+    const textToTranslate = nameElement.textContent;
+    const targetLanguage = language;
+  
+    translateText(textToTranslate, targetLanguage)
+      .then(translatedText => {
+        nameElement.textContent = translatedText;
+      })
+      .catch(error => {
+        console.error('Translation error:', error);
+      });
+  }
+  
+  function translateText(text, targetLanguage) {
+    const apiKey = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
+    const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+  
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: apiUrl,
+        type: 'POST',
+        data: {
+          q: text,
+          target: targetLanguage
+        },
+        success: function(response) {
+          if (response && response.data && response.data.translations && response.data.translations.length > 0) {
+            resolve(response.data.translations[0].translatedText);
+          } else {
+            reject('No translation available');
+          }
+        },
+        error: function(xhr, status, error) {
+          reject(error);
+        }
+      });
+    });
+  } */
